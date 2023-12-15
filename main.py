75b58f5d451abdc6d4599cf3942a90b83939618e
KingSlme <121655240+KingSlme@users.noreply.github.com>
2023-12-15 01:12:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import customtkinter
 from main_window import MainWindow
 import autoclicker
 
-def my_mainloop(root):
-    print("Hello World!")
-    root.after(1000, my_mainloop, root)  # run again after 1000ms (1s)
-
 if __name__ == "__main__":
     root = customtkinter.CTk()
     
