e4dc51b31796bae96b304b24a801f83cd95acac0
KingSlme <121655240+KingSlme@users.noreply.github.com>
2023-12-15 20:46:53 -0500

    Fixed consistency

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import customtkinter
 from main_window import MainWindow
-import autoclicker
+from autoclicker import Autoclicker
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
-    
-    ac = autoclicker.Autoclicker()
+    ac = Autoclicker()
     root.after(0, ac.autoclick, root)
     root.after(0, ac.set_varied_delay_loop, root)
     main_window = MainWindow(root, ac)
